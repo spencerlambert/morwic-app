6dae9650dc9d03d47275533ece0da2eca65fb5a5
sr-developer-remya <remya19@gmail.com>
2019-02-19 13:51:17 +0530

    change fields

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+       // Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Datatables' => Yajra\Datatables\Facades\Datatables::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+       // 'Image' => Intervention\Image\Facades\Image::class,
 
 
     ],
